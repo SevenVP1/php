27c373693da0ce1497c246cd25d24823f83e17c1
SevenVP1 <150113814+SevenVP1@users.noreply.github.com>
2024-05-07 14:38:45 +0700

    xuanle

---
+++ BMProject/MVC/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+
+class Home extends Controllers {
+    function displayIntroduction(){
+        echo "hello";
+    }
+    function displayUser(){
+        echo "welcome Le Hai Binh";
+    }
+}
+?>
